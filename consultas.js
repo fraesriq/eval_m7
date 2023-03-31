e1a1748736b4406ad7359bc11cd5b07006a4aa6f
Francisco Espindola <fraesri@gmail.com>
2023-03-31 20:10:27 -0300

    [NUEVO]Primera carga

--- consultas.js
+++ consultas.js
@@ -3,132 +3,109 @@ import Cursor from 'pg-cursor'
 const {Pool} = pkg;
 
 const pool = new Pool({
-    host: 'localhost',
-    database: 'ejemplo_cursores',
-    user: 'node',
-    password: '123456',
-    port: 5432,
-    max: 5,
-    idleTimeoutMillis: 3000,
-    connectionTimeoutMillis: 1000,
+	host: 'localhost',
+	database: 'eval_m7',
+	user: 'postgres',
+	password: '123456',
+	port: 5432,
+	max: 5,
+	idleTimeoutMillis: 3000,
+	connectionTimeoutMillis: 1000,
 })
 
 export const addPais = (nombre, continente, poblacion, pib_2019, pib_2020) => {
     
-    return new Promise(async (resolve, reject) => {
-        try {
+	return new Promise(async (resolve, reject) => {
+		try {
 
-            //primer paso begin
-            await pool.query('BEGIN');
-        
-            //segundo insertar registro tabla paises
-            await pool.query(`
-                INSERT INTO paises (nombre, continente, poblacion) VALUES($1, $2, $3)
-            `, [nombre, continente, poblacion]);
-        
-            //insertar registro en paises_pib
-            await pool.query(`
-                INSERT INTO paises_pib (nombre, pib_2019, pib_2020) VALUES($1, $2, $3)
-            `, [nombre, pib_2019, pib_2020]);
+			//primer paso begin
+			await pool.query('BEGIN');
+	
+			//segundo insertar registro tabla paises
+			await pool.query(`INSERT INTO paises (nombre, continente, poblacion) VALUES($1, $2, $3)`, [nombre, continente, poblacion]);
+	
+			//insertar registro en paises_pib
+			await pool.query(`INSERT INTO paises_pib (nombre, pib_2019, pib_2020) VALUES($1, $2, $3)`, [nombre, pib_2019, pib_2020]);
 
-            //validamos si ya existe pais registro en paises_data_web
-            let paisData = await pool.query("SELECT * FROM paises_data_web WHERE nombre_pais = $1", [nombre]);
-            if(paisData.rows.length == 0){
-            //crear registro en paises_data_web
-                await pool.query(`
-                    INSERT INTO paises_data_web (nombre_pais, accion) VALUES($1, $2)
-                `, [nombre, 1]);
-            }else {
-                //hacemos update en paises_data_web
+			//validamos si ya existe pais registro en paises_data_web
+			let paisData = await pool.query("SELECT * FROM paises_data_web WHERE nombre_pais = $1", [nombre]);
 
-                await pool.query(`
-                    UPDATE paises_data_web SET accion = 1 WHERE nombre_pais = $1
-                `, [nombre])
-            }
-    
-        
-            //FINALMENTE GURDAMOS LOS CAMBIOS
-            await pool.query('COMMIT')
-            resolve("País agregado correctamente.")
-            
-           } catch (error) {
-            await pool.query('ROLLBACK')
-            reject("Error al ingresar el país.")
-           }
-    })
+			if(paisData.rows.length == 0){
+				//crear registro en paises_data_web
+				await pool.query(`INSERT INTO paises_data_web (nombre_pais, accion) VALUES($1, $2)`, [nombre, 1]);
+			}else {
+				//hacemos update en paises_data_web
+				await pool.query(`UPDATE paises_data_web SET accion = 1 WHERE nombre_pais = $1`, [nombre])
+			}
+	
+			//FINALMENTE GURDAMOS LOS CAMBIOS
+			await pool.query('COMMIT')
+			resolve("País agregado correctamente.")
+				
+		} catch (error) {
+			await pool.query('ROLLBACK')
+			reject("Error al ingresar el país.")
+		}
+	})
 }
 
 
 export const deletePais = (nombre) => {
-    return new Promise(async (resolve, reject) => {
-        try {
+	return new Promise(async (resolve, reject) => {
+		try {
 
-            //primer paso begin
-            await pool.query('BEGIN');
+			//primer paso begin
+			await pool.query('BEGIN');
 
-            //validar si pais existe
-            let pais = await pool.query("SELECT * FROM paises WHERE nombre = $1", [nombre]);
-            if(pais.rows.length == 0) throw new Error("No existe registrado el país: " + nombre);
-        
-            //insertar registro en paises_pib
-            await pool.query(`
-            DELETE FROM paises_pib WHERE nombre = $1
-            `, [nombre]);
+			//validar si pais existe
+			let pais = await pool.query("SELECT * FROM paises WHERE nombre = $1", [nombre]);
+			if(pais.rows.length == 0) throw new Error("No existe registrado el país: " + nombre);
+	
+			//insertar registro en paises_pib
+			await pool.query(`DELETE FROM paises_pib WHERE nombre = $1`, [nombre]);
 
-            //insertar registro tabla paises
-            await pool.query(`
-                DELETE FROM paises WHERE nombre = $1
-            `, [nombre]);
-        
+			//insertar registro tabla paises
+			await pool.query(`DELETE FROM paises WHERE nombre = $1`, [nombre]);
+	
 
-            //validar si existe un registo previo en la tabla paises_data_web
+			//validar si existe un registo previo en la tabla paises_data_web
 
-            //validar si pais existe
-            let paisData = await pool.query("SELECT * FROM paises_data_web WHERE nombre_pais = $1", [nombre]);
-            if(paisData.rows.length == 0){
-                //crear registro en paises_data_web
-
-                await pool.query(`
-                INSERT INTO paises_data_web (nombre_pais, accion) VALUES($1, $2)
-                `, [nombre, 0])
-            }else{
-                //hacemos update en paises_data_web
-
-                await pool.query(`
-                    UPDATE paises_data_web SET accion = 0 WHERE nombre_pais = $1
-                `, [nombre])
-            }
-        
-            //FINALMENTE GURDAMOS LOS CAMBIOS
-            await pool.query('COMMIT')
-            resolve("País eliminado correctamente.")
-            
-           } catch (error) {
-            await pool.query('ROLLBACK')
-            console.log(error)
-            reject("error al eliminar el pais: "+nombre)
-           }
-    })
+			//validar si pais existe
+			let paisData = await pool.query("SELECT * FROM paises_data_web WHERE nombre_pais = $1", [nombre]);
+			if(paisData.rows.length == 0){
+				//crear registro en paises_data_web
+				await pool.query(`INSERT INTO paises_data_web (nombre_pais, accion) VALUES($1, $2)`, [nombre, 0])
+			}else{
+				//hacemos update en paises_data_web
+				await pool.query(`UPDATE paises_data_web SET accion = 0 WHERE nombre_pais = $1`, [nombre])
+			}
+	
+			//FINALMENTE GURDAMOS LOS CAMBIOS
+			await pool.query('COMMIT')
+			resolve("País eliminado correctamente.")
+				
+		} catch (error) {
+			await pool.query('ROLLBACK')
+			console.log(error)
+			reject("error al eliminar el pais: "+nombre)
+		}
+	})
 }
 
 
-export const filtrarPaises = (limit, offset) => {
-    console.log(limit, offset)
-    return new Promise(async (resolve, reject) => {
-
-        let query = `
-            SELECT p.nombre, p.continente, p.poblacion, pp.pib_2019, pp.pib_2020 FROM paises p
-            join paises_pib pp
-            on p.nombre = pp.nombre
-            limit ${limit} offset ${offset}
-            `
-            console.log(query)
-        try {
-            let paises = await pool.query(query)
-            resolve(paises.rows)
-            
-           } catch (error) {
-            reject("Error al filtrar los países")
-           }
-    })
+export const filtrarPaises = (limit, offset) => {	
+	return new Promise(async (resolve, reject) => {
+		let query = `
+			SELECT p.nombre, p.continente, p.poblacion, pp.pib_2019, pp.pib_2020 FROM paises p
+			join paises_pib pp
+			on p.nombre = pp.nombre
+			limit ${limit} offset ${offset}
+			`
+		try {
+			let paises = await pool.query(query)
+			resolve(paises.rows)
+		} catch (error) {
+			reject("Error al filtrar los países")
+		}
+	})
 }
--- index.js
+++ index.js
@@ -31,22 +31,23 @@ app.listen(PORT, () => console.log("http://localhost:3000"));
 
 //RUTAS DE VISTAS
 app.get(["/", "/home"], async (req, res) => {
-        let limit = req.query.limit || 2;
-        let offset = req.query.offset || 0;
-    filtrarPaises(limit, offset).then(paises => {
-        console.log(paises)
-       if(paises.length == 0){
-        ultimoOffset = 0;
-       }
-        res.render("home", {
-            paises,
-            ultimoOffset: offset 
-        });
-    }).catch(error => {
-        res.render("home", {
-            error
-        });
-    })
+	let limit = req.query.limit || 2;
+	let offset = req.query.offset || 0;
+	
+	filtrarPaises(limit, offset).then(paises => {
+
+		if(paises.length == 0){
+			ultimoOffset = 0;
+		} 
+		res.render("home", {
+			paises,
+			ultimoOffset: offset 
+		});
+	}).catch(error => {
+			res.render("home", {
+					error
+			});
+	})
 })
 
 
@@ -57,26 +58,42 @@ app.get("/add/pais", (req, res) => {
 
 //RUTA RECIBE DATOS DE PAISES
 app.post("/add/pais", async (req, res) => {
-    let {nombre, continente, poblacion, pib_2019, pib_2020} = req.body;
-    addPais(nombre, continente, poblacion, pib_2019, pib_2020).then(respuesta => {
-        res.send("<p>Registro salío bien: <a href='http://localhost:3000'>Volver al inicio</a></p>")
-    }).catch(error => {
-        res.send("<p>no se pudo realizar el Registro: <a href='http://localhost:3000/add/pais'>Volver</a></p>")
-    })
+	let {nombre, continente, poblacion, pib_2019, pib_2020} = req.body;
+	addPais(nombre, continente, poblacion, pib_2019, pib_2020).then(respuesta => {		
+		let limit = req.query.limit || 2;
+		let offset = req.query.offset || 0;
+		filtrarPaises(limit, offset).then(paises => {
+			console.log(paises)
+			if(paises.length == 0){
+				ultimoOffset = 0;
+			}
+			res.render("home", {
+				paises,
+				ultimoOffset: offset 
+			});
+		}).catch(error => {
+				res.render("home", {
+						error
+				});
+		})
+		// res.send("<p>Registro salío bien: <a href='http://localhost:3000'>Volver al inicio</a></p>")
+	}).catch(error => {
+		res.send("<p>no se pudo realizar el Registro: <a href='http://localhost:3000/add/pais'>Volver</a></p>")
+	})
 });
 
 //RUTA ELIMINAR PAIS VISTA
 app.get("/delete/pais", async (req, res) => {
-    res.render("deletePais");
+	res.render("deletePais");
 });
 
 //RUTA ELIMINAR PAIS ENDPOINT
 app.delete("/delete/pais/:nombre", async (req, res) => {
-    let nombre = req.params.nombre;
-    deletePais(nombre).then(respuesta => {
-        res.json({code: 200, message: respuesta})
-    }).catch(error => {
-        res.status(500).json({code: 500, error})
-    })
+	let nombre = req.params.nombre;
+	deletePais(nombre).then(respuesta => {
+		res.json({code: 200, message: respuesta})
+	}).catch(error => {
+		res.status(500).json({code: 500, error})
+	})
 
 });
